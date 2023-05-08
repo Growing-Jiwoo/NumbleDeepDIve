507d0a7b0fcccf2f673b846777b5f17b8ed47deb
Growing-Jiwoo <fbaa4412@naver.com>
2023-05-08 16:28:24 +0900

    feat: react-router-dom 적용 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import LoginUi from "./Components/Login/LoginUi";
+import SignUp from "./Components/Login/SignUp";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        padding: "60px 16px 16px",
+        maxWidth: "720px",
+      }}
+    >
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginUi />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+      </div>
     </div>
   );
 }
