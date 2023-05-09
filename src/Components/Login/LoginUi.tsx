efcab13ac0517da5f8a08d6ad27c3b30b3f08388
Growing-Jiwoo <fbaa4412@naver.com>
2023-05-09 21:09:17 +0900

    fix: 로그인 및 회원가입 성공&실패 처리 및 일부 UI 코드 정리 및 변경 (#6)

--- src/Components/Login/LoginUi.tsx
+++ src/Components/Login/LoginUi.tsx
@@ -1,54 +1,107 @@
-import { useState } from "react";
-import { Container, Form, Input, Button, LogoImage } from "./styled";
+import { useEffect, useState } from "react";
+import { Container, Form, Input, Button } from "./styled";
 import { useCookies } from "react-cookie";
 import useAxiosWithAuth from "../../Hooks/useAxiosWithAuth";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface Login {
+  userId: string | number;
+  password: string | number;
+}
 
 function LoginUi(): JSX.Element {
   const axiosInstance = useAxiosWithAuth();
   const navigate = useNavigate();
   const [cookies, setCookie] = useCookies(["jwt"]);
-  const [values, setValues] = useState({
+  const [values, setValues] = useState<Login>({
     userId: "",
     password: "",
   });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+    setError,
+  } = useForm<Login>({
+    criteriaMode: "all",
+    mode: "onChange",
+  });
 
-  const handleChange = (e: any) => {
+  useEffect(() => {
     setValues({
-      ...values,
-      [e.target.name]: e.target.value,
+      userId: watch("userId") ?? "",
+      password: watch("password") ?? "",
     });
-  };
+  }, [watch]);
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
+  const onSubmit = async (values: Login) => {
     try {
-      const response = await axiosInstance.post("users/login/", values);
-      setCookie("jwt", response.data.token);
-      // navigate("/home");
-    } catch (error) {
-      throw "로그인 실패";
+      console.log(values);
+      const response = await axiosInstance.post("auth/signin/", values);
+      setCookie("jwt", response.data.accessToken);
+      navigate("/Main");
+    } catch (error: any) {
+      console.log(error);
+      if (error.response?.status === 400) {
+        setError("userId", {
+          type: "manual",
+          message: "가입하지 않았거나 아이디나 비밀번호가 잘못되었습니다.",
+        });
+      } else {
+        throw new Error("회원가입 실패");
+      }
     }
   };
 
   return (
     <Container>
-      <Form onSubmit={handleSubmit}>
+      <h1>로그인</h1>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Input
           type="text"
           placeholder="아이디 (영소문자, 숫자 3~10자)"
-          name="userId"
-          // value={values.userId}
-          // onChange={handleChange}
+          value={watch("userId") ?? ""}
+          {...register("userId", {
+            required: "아이디는 필수 입력 사항입니다.",
+            pattern: {
+              value: /^[a-z0-9]{3,10}$/i,
+              message: "아이디는 영소문자와 숫자 3~10자리로 이루어져야 합니다.",
+            },
+          })}
         />
+        {errors.userId && <span>{errors.userId.message}</span>}
+
         <Input
           type="password"
           placeholder="비밀번호 (3~10자)"
-          name="password"
-          // value={values.password}
-          // onChange={handleChange}
+          value={watch("password") ?? ""}
+          {...register("password", {
+            required: "비밀번호는 필수 입력 사항입니다.",
+            minLength: {
+              value: 3,
+              message: "비밀번호는 3자리 이상이어야 합니다.",
+            },
+            maxLength: {
+              value: 10,
+              message: "비밀번호는 10자리 이하여야 합니다.",
+            },
+          })}
         />
-        <Button className="login">로그인</Button>
+        {errors.password && <span>{errors.password.message}</span>}
+        <Button
+          className="login"
+          disabled={!isValid}
+          style={{
+            backgroundColor: isValid ? "transparent" : "grey",
+            color: isValid ? "rgb(25, 118, 210)" : "white",
+            cursor: isValid ? "pointer" : "auto",
+          }}
+        >
+          로그인
+        </Button>
+
         <Button
           className="signup"
           onClick={() => {
