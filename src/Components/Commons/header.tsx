2f6b30e8aedbcb2b0226ca7b57a21325215f27a0
Growing-Jiwoo <fbaa4412@naver.com>
2023-05-10 18:34:55 +0900

    feat: 페이지 뒤로가기 기능 구현 (#15)

--- src/Components/Commons/header.tsx
+++ src/Components/Commons/header.tsx
@@ -1,17 +1,25 @@
 import { useNavigate } from "react-router-dom";
-import { Header, HeaderContainer, HeaderText } from "./styled";
+import { Header, HeaderContainer, HeaderImage, HeaderText } from "./styled";
 
-function Main(): JSX.Element {
+function HeaderUi(): JSX.Element {
   const navigate = useNavigate();
+
+  const handleImageClick = () => {
+    navigate(-1);
+  };
 
   return (
     <Header>
+      <HeaderImage
+        src={`${process.env.PUBLIC_URL}/img/backbtn.PNG`}
+        alt="Back Button"
+        onClick={handleImageClick}
+      />
       <HeaderContainer>
-        <img alt="backbtn" src={`${process.env.PUBLIC_URL}/img/backbtn.PNG`} />
         <HeaderText>안녕하세요</HeaderText>
       </HeaderContainer>
     </Header>
   );
 }
 
-export default Main;
+export default HeaderUi;
