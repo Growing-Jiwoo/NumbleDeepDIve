57493a845cecdbdf94ebb7d422a2fe980b9066a9
Growing-Jiwoo <fbaa4412@naver.com>
2023-05-10 18:07:55 +0900

    fix: UserProfile 관련 컴포넌트 분리 및 UI 코드 일부 수정

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,48 +1,29 @@
 import { useEffect, useState } from "react";
 import useAxiosWithAuth from "../../Hooks/useAxiosWithAuth";
-import { useNavigate } from "react-router-dom";
-import {
-  Button,
-  ButtonContainer,
-  Container,
-  ProfileBox,
-  ProfileImg,
-  UserName,
-} from "./styled";
+import type { UserData } from "../../Interface/interface";
+import { Container, ProfileBox } from "./styled";
+import UserProfile from "./UserProfile";
 
 function Main(): JSX.Element {
   const axiosInstance = useAxiosWithAuth();
-  const navigate = useNavigate();
+  const [userList, setUserList] = useState<UserData[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await axiosInstance.get("/member/list");
-        console.log(response);
+        setUserList(response.data);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, [axiosInstance]);
+  }, []);
 
   return (
     <Container>
       <ProfileBox>
-        <ProfileImg>
-          <svg width="400" height="400">
-            <image
-              href="https://images.mypetlife.co.kr/content/uploads/2018/12/09154907/cotton-tulear-2422612_1280.jpg"
-              width="100%"
-              height="100%"
-            />
-          </svg>
-        </ProfileImg>
-        <UserName>hi</UserName>
-        <ButtonContainer>
-          <Button id="likeBtn">좋아요</Button>
-          <Button id="hateBtn">싫어요</Button>
-        </ButtonContainer>
+        {userList.length > 0 && <UserProfile userList={userList} />}
       </ProfileBox>
     </Container>
   );
