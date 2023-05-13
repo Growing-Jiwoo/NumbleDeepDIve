3ba03caa1f1befff488c2349c76a09eae255ac9e
Growing-Jiwoo <fbaa4412@naver.com>
2023-05-14 00:11:33 +0900

    feat: Myinfo UI 및 세부 기능 구현 (#34)

---
+++ src/Components/Myinfo/Myinfo.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useEffect, useState } from "react";
+import useAxiosWithAuth from "../../Hooks/useAxiosWithAuth";
+import {
+  ButtonCotainer,
+  ButtonDiv,
+  Container,
+  EditBtn,
+  MyInfoProfile,
+  MyInfoProfileImg,
+  MyinfoEdit,
+  NameText,
+} from "./styled";
+import type { MyInfoValue, UserData } from "../../Interface/interface";
+import UserProfileImg from "../Commons/UserProfileImg";
+import EditPopup from "./EditPopup";
+import { useNavigate } from "react-router-dom";
+
+export default function Myinfo() {
+  const navigate = useNavigate();
+  const axiosInstance = useAxiosWithAuth();
+  const [values, setValues] = useState<MyInfoValue>({
+    nickname: "",
+    profileImgUrl: "",
+  });
+
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
+  const openEditModal = () => setIsEditOpen(true);
+  const closeEditModal = () => setIsEditOpen(false);
+
+  async function fetchData() {
+    try {
+      const response = await axiosInstance.get<UserData>("/auth/user");
+      setValues({
+        nickname: response.data.nickname,
+        profileImgUrl: response.data.profileImgUrl,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(values);
+
+  const onSearch = useCallback(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <Container>
+      <MyInfoProfile>
+        <MyInfoProfileImg>
+          <UserProfileImg profileImgUrl={values.profileImgUrl} />
+        </MyInfoProfileImg>
+        <MyinfoEdit>
+          <NameText>{values.nickname}</NameText>
+          <EditBtn onClick={openEditModal}>
+            <img
+              src={`${process.env.PUBLIC_URL}/img/editicon.png`}
+              alt="editBtn"
+            />
+          </EditBtn>
+        </MyinfoEdit>
+      </MyInfoProfile>
+      <ButtonCotainer>
+        <ButtonDiv>내가 좋아한 사람</ButtonDiv>
+        <ButtonDiv>나를 좋아한 사람</ButtonDiv>
+        <ButtonDiv>로그아웃</ButtonDiv>
+      </ButtonCotainer>
+
+      {isEditOpen && (
+        <EditPopup
+          isOpen={isEditOpen}
+          closeModal={closeEditModal}
+          nickname={values.nickname}
+          onSearch={onSearch}
+        />
+      )}
+    </Container>
+  );
+}
